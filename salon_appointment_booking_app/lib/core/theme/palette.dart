d3a24b0de4db76f48ed867ae6581a7dba3c09e29
DULAJBHAGYA <dulajupananda@gmail.com>
2024-07-09 17:04:57 +0530

    initial commit
    Developed home page

---
+++ salon_appointment_booking_app/lib/core/theme/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color backgroundColor = Color.fromARGB(255, 243, 239, 239);
+  static const Color whiteColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color blackColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color errorColor = Colors.redAccent;
+  static const Color greyColor = Colors.grey;
+  static const Color mainColor = Color(0xFF898970);
+  static const Color brownColor = Color(0xFF83473e);
+}
---
+++ salon_appointment_booking_app/lib/core/theme/theme.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:salon_appointment_booking_app/core/theme/palette.dart';
+
+class AppTheme {
+  static final lightThemeMode = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: Palette.backgroundColor,
+    textTheme: GoogleFonts.urbanistTextTheme(
+      ThemeData.light().textTheme,
+    ).copyWith(
+      displayLarge: GoogleFonts.urbanist(
+        textStyle: TextStyle(
+          fontSize: 30,
+          color: Palette.blackColor,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+      headlineLarge: GoogleFonts.urbanist(
+        textStyle: TextStyle(
+          fontSize: 24,
+          color: Palette.blackColor,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      headlineMedium: GoogleFonts.urbanist(
+        textStyle: TextStyle(
+          fontSize: 20,
+          color: Palette.blackColor,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      headlineSmall: GoogleFonts.urbanist(
+        textStyle: TextStyle(
+          fontSize: 18,
+          color: Palette.blackColor,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      bodyMedium: GoogleFonts.urbanist(
+        textStyle: TextStyle(
+          fontSize: 16,
+          color: Palette.brownColor,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      titleMedium: GoogleFonts.urbanist(
+        textStyle: TextStyle(
+          fontSize: 14,
+          color: Palette.greyColor,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      bodySmall: GoogleFonts.urbanist(
+        textStyle: TextStyle(
+          fontSize: 14,
+          color: Palette.blackColor,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+    ),
+  );
+}
